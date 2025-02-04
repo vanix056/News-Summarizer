dcbd2ee948b2bc38d35e9b1bad8d655a7e9ce0fb
vanix056 <abdullahwaqar121105@gmail.com>
2025-02-04 05:07:37 +0500

    Logic Completed

--- app.py
+++ app.py
@@ -16,6 +16,34 @@ def summarize():
     summary.config(state='normal')
     sentiment.config(state='normal')
     
+    title.delete('1.0','end')
+    title.insert('1.0',article.title)
+    
+    author.delete('1.0','end')
+    author.insert('1.0',article.authors)
+    
+    publication.delete('1.0','end')
+    publication.insert('1.0',article.publish_date)
+    
+    summary.delete('1.0','end')
+    summary.insert('1.0',article.summary)
+    
+    analysis=TextBlob(article.text)
+    sentiment.delete('1.0','end')
+    sentiment.insert('1.0',f'Polarity: {analysis.polarity}, Sentiment: {"Positive" if analysis.polarity > 0 else "Negative" if analysis.polarity < 0 else "Neutral"}')
+    
+    title.config(state='disabled')
+    author.config(state='disabled')
+    publication.config(state='disabled')
+    summary.config(state='disabled')
+    sentiment.config(state='disabled')
+    
+    
+gui=t.TK()
+gui.title('News Summary Report')
+
+    
+    
     
 
 
